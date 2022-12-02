82da57d015c75fdd8c016a9a19db762fbba14d47
Amarnath <113051198+AmarnathBheema@users.noreply.github.com>
2022-12-02 15:03:14 +0530

    Update word.sh

--- word.sh
+++ word.sh
@@ -8,7 +8,6 @@ sudo apt-get -y install docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 cd local/
-mv docker-compose.yml /home/ubuntu
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker-compose up -d
