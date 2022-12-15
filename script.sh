d789ecf1c984fbe916284df2cc045ea1b4045e1f
Amarnath <113051198+AmarnathBheema@users.noreply.github.com>
2022-12-15 17:00:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ sudo apt-get install  git -y
 sudo git clone https://github.com/AmarnathBheema/dock.git
 sudo apt-get update -y
 sudo apt-get install docker.io -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo apt-get -y install docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
